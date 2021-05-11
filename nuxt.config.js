7300a355cb481eecee04a49d64da498c58afe1ef
Emmanuel Nelson <elnels@gmail.com>
2021-05-11 11:11:22 -0400

    update contact form

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
   ],
-  ssr: false,
+  ssr: true,
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
